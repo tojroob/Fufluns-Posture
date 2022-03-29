4fd652f7ff092de2528474de83c1e36f10108f9a
Dapo <Dapo.owolabi@outlook.com>
2022-03-28 22:09:34 -0400

    Logic

--- Fufluns Posture/Logic.swift
+++ Fufluns Posture/Logic.swift
@@ -13,7 +13,7 @@ class User: ObservableObject {
     
     @Published var name: String = "Hello User"
     @Published var notificationcount: [Int] = [5,10, 15, 20, 25]
-    @Published var nOfNotifications: Int = 0
+    @Published var nOfNotifications: Int = 0 // Value changes when user interacts app button (yes)
     @Published var startTime: Date = Calendar.current.date(bySettingHour: 8, minute: 0, second: 0, of: Date())!
     @Published var endTime: Date = Calendar.current.date(bySettingHour: 22, minute: 0, second: 0, of: Date())!
     
