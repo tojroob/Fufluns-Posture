ba9101ae92071f0bb9b64c5bb41b537b1d2ee20f
Dapo <Dapo.owolabi@outlook.com>
2022-03-28 21:41:07 -0400

    ok

--- Fufluns Posture/ContentView.swift
+++ Fufluns Posture/ContentView.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Github!")
-            .padding()
-        Text("Hello again Github.")
-        Text("Commit again.")
+        VStack{
+            Text("Hello")
+            Button("red"){}
+        }
     }
 }
 
