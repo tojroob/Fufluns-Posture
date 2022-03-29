af85b5c0853326eca9e0153a74976241173b3e36
James <jytojroob@gmail.com>
2022-03-28 18:08:16 -0700

    Initial Commit

---
+++ Fufluns Posture/Fufluns_PostureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fufluns_PostureApp.swift
+//  Fufluns Posture
+//
+//  Created by James Yang on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Fufluns_PostureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
