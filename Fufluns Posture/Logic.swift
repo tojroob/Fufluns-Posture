5718bce6a2871972fc9bd35b2e4e27dfbea2ffc2
Dapo <Dapo.owolabi@outlook.com>
2022-03-28 22:29:51 -0400

    Logic

--- Fufluns Posture/Logic.swift
+++ Fufluns Posture/Logic.swift
@@ -5,21 +5,22 @@
 //  Created by Daps Owolabi on 2022-03-28.
 //
 
-import Foundation
+import SwiftUI
+import UserNotifications
 
 class User: ObservableObject {
     
   
     
     @Published var name: String = "Hello User"
-    @Published var notificationcount: [Int] = [5,10, 15, 20, 25]
+    @Published var notificationcount: Int
     @Published var nOfNotifications: Int = 0 // Value changes when user interacts app button (yes)
     @Published var startTime: Date = Calendar.current.date(bySettingHour: 8, minute: 0, second: 0, of: Date())!
     @Published var endTime: Date = Calendar.current.date(bySettingHour: 22, minute: 0, second: 0, of: Date())!
     
     
-    init () {
-    
+    init (count: Int) {
+        self.notificationcount = count
     }
     
     
